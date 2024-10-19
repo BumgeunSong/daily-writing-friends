05c3783b7f53184ff164112db277ca53eb3739e7
BumgeunSong <isp1195@gmail.com>
2024-10-19 18:40:57 +0900

    Set up router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
-import { signOut } from './firebase';
+import FeedPage from './components/FeedPage';
+import PostCreationPage from './components/PostCreationPage';
+import PostDetailPage from './components/PostDetailPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Artico</h1>
-      <p>Firebase가 구성되었습니다!</p>
-      <LoginPage />
-      <button onClick={signOut}>로그아웃</button>
-    </div>
+    <Switch>
+      <Route exact path="/login" component={LoginPage} />
+      <PrivateRoute exact path="/feed" component={FeedPage} />
+      <PrivateRoute exact path="/create" component={PostCreationPage} />
+      <PrivateRoute exact path="/post/:id" component={PostDetailPage} />
+      <Redirect to="/feed" />
+    </Switch>
   );
 }
 
