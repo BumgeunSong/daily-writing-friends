e73d2ec859b1acb44a44839eebbaf260b6430feb
BumgeunSong <isp1195@gmail.com>
2024-10-20 15:50:12 +0900

    원격에서 github secret을 사용하도록 한다

--- src/firebase.ts
+++ src/firebase.ts
@@ -10,7 +10,7 @@ import {
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
+  apiKey: process.env.VITE_FIREBASE_API_KEY as string,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN as string,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID as string,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET as string,
