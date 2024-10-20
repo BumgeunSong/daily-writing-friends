9efd97666fb032410a6e695917a2fe738089caf8
BumgeunSong <isp1195@gmail.com>
2024-10-20 15:14:30 +0900

    BrowerRouter 컴포넌트 감싸기

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
+import { BrowserRouter, Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
         <App />
-    </AuthProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
