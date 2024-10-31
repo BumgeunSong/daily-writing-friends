49d58d914e5de04f60036f1477fae7fe75c879c5
BumgeunSong <isp1195@gmail.com>
2024-10-31 15:27:56 +0900

    console log 제거

--- src/components/Pages/RecentBoard.tsx
+++ src/components/Pages/RecentBoard.tsx
@@ -4,12 +4,10 @@ import { Navigate } from 'react-router-dom';
 const RecentBoard: React.FC = () => {
   const boardId = localStorage.getItem('boardId');
 
-  console.log("boardId", boardId)
   if (boardId) {
     return <Navigate to={`/board/${boardId}`} />;
   }
 
-  console.log("/boards/list")
   return <Navigate to="/boards/list" />;
 };
 
