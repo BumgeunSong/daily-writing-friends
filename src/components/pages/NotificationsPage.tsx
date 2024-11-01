89ffd25f0dfbc9c77743df722dc40fb8e489b19e
BumgeunSong <isp1195@gmail.com>
2024-11-01 18:52:10 +0900

    알림 기능 공사중 표시

--- src/components/pages/NotificationsPage.tsx
+++ src/components/pages/NotificationsPage.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const NotificationsPage: React.FC = () => {
-  return <div>NotificationPage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <span className="text-6xl mb-4">🚧</span>
+      <p className="text-xl font-semibold text-gray-700">알림 기능은 아직 준비중이에요.</p>
+    </div>
+  );
 };
 
 export default NotificationsPage;
