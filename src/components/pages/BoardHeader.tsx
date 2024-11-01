50430c84caddde21e49765a1b65c7852572b0d50
BumgeunSong <isp1195@gmail.com>
2024-11-01 19:04:57 +0900

    Header에서는 로그아웃 버튼 지우기

--- src/components/pages/BoardHeader.tsx
+++ src/components/pages/BoardHeader.tsx
@@ -1,25 +1,12 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { Button } from '../ui/button';
-import { LogOut, ChevronDown } from 'lucide-react';
-import { signOut } from 'firebase/auth';
-import { auth } from '@/firebase';
+import { Link } from 'react-router-dom'
+import { ChevronDown } from 'lucide-react';
 
 interface BoardHeadderProps {
     title: string;
 }
 
 const BoardHeader: React.FC<BoardHeadderProps> = ({ title }) => {
-    const navigate = useNavigate();
-    const handleLogout = async () => {
-        try {
-            await signOut(auth);
-            navigate('/login'); // 로그아웃 후 로그인 페이지로 이동
-        } catch (error) {
-            console.error('로그아웃 오류:', error);
-        }
-    };
-
     return (
         <header className="bg-primary text-primary-foreground py-4">
             <div className="container mx-auto px-4 flex justify-between items-center">
@@ -30,15 +17,6 @@ const BoardHeader: React.FC<BoardHeadderProps> = ({ title }) => {
                     <span className="text-2xl font-bold sm:text-3xl">{title}</span>
                     <ChevronDown className="h-5 w-5" />
                 </Link>
-                <Button
-                    variant="ghost"
-                    size="icon"
-                    onClick={handleLogout}
-                    className="text-primary-foreground hover:bg-primary-foreground hover:text-primary"
-                    aria-label="Sign out"
-                >
-                    <LogOut className="h-5 w-5" />
-                </Button>
             </div>
         </header>
     )
