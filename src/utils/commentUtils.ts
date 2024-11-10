843c2bb5ed8115281ae8da8a502247c544a4d53f
changhoe kim <kormelon@gmail.com>
2024-11-10 14:16:07 +0900

    feat: 댓글/대댓글 수정/삭제 api 추가

--- src/utils/commentUtils.ts
+++ src/utils/commentUtils.ts
@@ -1,38 +1,108 @@
-import { firestore } from '../firebase';
-import { collection, addDoc, doc, serverTimestamp } from 'firebase/firestore';
+import { firestore } from "../firebase";
+import {
+  collection,
+  addDoc,
+  doc,
+  serverTimestamp,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
 
-// Function to add a comment to a post
-const addCommentToPost = async (postId: string, content: string, userId: string, userName: string, userProfileImage: string) => {
+export const addCommentToPost = async (
+  postId: string,
+  content: string,
+  userId: string,
+  userName: string,
+  userProfileImage: string
+) => {
   try {
-    const postRef = doc(firestore, 'posts', postId);
-    await addDoc(collection(postRef, 'comments'), {
-        content,
-        userId,
-        userName,
-        userProfileImage,
-        createdAt: serverTimestamp(),  
+    const postRef = doc(firestore, "posts", postId);
+    await addDoc(collection(postRef, "comments"), {
+      content,
+      userId,
+      userName,
+      userProfileImage,
+      createdAt: serverTimestamp(),
     });
-    console.log('Comment added successfully');
+    console.log("Comment added successfully");
   } catch (error) {
-    console.error('Error adding comment:', error);
+    console.error("Error adding comment:", error);
   }
 };
 
-// Function to add reply to a comment
-const addReplyToComment = async (postId: string, commentId: string, content: string, userId: string, userName: string, userProfileImage: string) => {
+export const updateCommentToPost = async (
+  postId: string,
+  commentId: string,
+  content: string
+) => {
   try {
-    const postRef = doc(firestore, 'posts', postId);
-    await addDoc(collection(postRef, 'comments', commentId, 'replies'), {
-        content,
-        userId,
-        userName,
-        userProfileImage,
-        createdAt: serverTimestamp(),  
-    });
-    console.log('Reply added successfully');
+    const postRef = doc(firestore, "posts", postId);
+    await updateDoc(doc(postRef, "comments", commentId), { content });
   } catch (error) {
-    console.error('Error adding reply:', error);
+    console.error("Error updating comment:", error);
   }
-}
+};
 
-export { addCommentToPost, addReplyToComment };
+export const deleteCommentToPost = async (
+  postId: string,
+  commentId: string
+) => {
+  try {
+    const postRef = doc(firestore, "posts", postId);
+    await deleteDoc(doc(postRef, "comments", commentId));
+  } catch (error) {
+    console.error("Error deleting comment:", error);
+  }
+};
+
+export const addReplyToComment = async (
+  postId: string,
+  commentId: string,
+  content: string,
+  userId: string,
+  userName: string,
+  userProfileImage: string
+) => {
+  try {
+    const postRef = doc(firestore, "posts", postId);
+    await addDoc(collection(postRef, "comments", commentId, "replies"), {
+      content,
+      userId,
+      userName,
+      userProfileImage,
+      createdAt: serverTimestamp(),
+    });
+    console.log("Reply added successfully");
+  } catch (error) {
+    console.error("Error adding reply:", error);
+  }
+};
+
+export const updateReplyToComment = async (
+  postId: string,
+  commentId: string,
+  replyId: string,
+  content: string
+) => {
+  try {
+    const postRef = doc(firestore, "posts", postId);
+    await updateDoc(doc(postRef, "comments", commentId, "replies", replyId), {
+      content,
+    });
+  } catch (error) {
+    console.error("Error updating reply:", error);
+  }
+};
+
+export const deleteReplyToComment = async (
+  postId: string,
+  commentId: string,
+  replyId: string
+) => {
+  try {
+    const postRef = doc(firestore, "posts", postId);
+    await deleteDoc(doc(postRef, "comments", commentId, "replies", replyId));
+  } catch (error) {
+    console.error("Error deleting reply:", error);
+  }
+};
