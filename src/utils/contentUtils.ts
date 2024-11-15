ab1ebb3d607ce46199b031f2be740f008a97470b
BumgeunSong <isp1195@gmail.com>
2024-11-15 19:19:58 +0900

    http가 없어도 url을 변환해주도록 regex 수정

--- src/utils/contentUtils.ts
+++ src/utils/contentUtils.ts
@@ -1,6 +1,9 @@
 function convertUrlsToLinks(content: string): string {
-    const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
-    return content.replace(urlRegex, '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>');
+    const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|]|\bwww\.[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|]|\b[-A-Z0-9+&@#\/%?=~_|!:,.;]+\.[A-Z]{2,4}\b)/gi;
+    return content.replace(urlRegex, (url) => {
+        const href = url.startsWith('http') ? url : `http://${url}`;
+        return `<a href="${href}" target="_blank" rel="noopener noreferrer">${url}</a>`;
+    });
 }
 
 export { convertUrlsToLinks };
