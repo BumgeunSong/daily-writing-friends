ba4ea41f64f5069c313254708faa0fb83130d35d
BumgeunSong <isp1195@gmail.com>
2024-11-22 13:05:19 +0900

    ProtectedRoute로 왔을 때 유저가 로그인 상태인데 설정해둔 redirectPath가 있으면 이동하게 한다

--- src/components/route/ProtectedRoute.tsx
+++ src/components/route/ProtectedRoute.tsx
@@ -1,11 +1,21 @@
 import { useAuth } from '../../contexts/AuthContext';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const { currentUser } = useAuth();
+  const { currentUser, redirectPathAfterLogin, setRedirectPathAfterLogin } = useAuth();
+  const location = useLocation();
 
+  // If the user is not logged in, redirect to login and store the current path
   if (!currentUser) {
+    setRedirectPathAfterLogin(location.pathname);
     return <Navigate to="/login" />;
+  }
+
+  // If the user is logged in but has a redirect path, navigate to it
+  if (currentUser && redirectPathAfterLogin) {
+    const redirectTo = redirectPathAfterLogin;
+    setRedirectPathAfterLogin(null);
+    return <Navigate to={redirectTo} />;
   }
 
   return children;
