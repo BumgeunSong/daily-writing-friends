1356acc084b011f17b5d016bc89a9981a4e93e50
BumgeunSong <isp1195@gmail.com>
2024-11-30 22:03:33 +0900

    eslint/prettier 동일한 설정으로 맞추기

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -3,18 +3,23 @@ module.exports = {
   env: {
     es6: true,
     node: true,
+    browser: true,
   },
   extends: [
     "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+    "plugin:tailwindcss/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: ["tsconfig.json", "tsconfig.dev.json"],
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
     sourceType: "module",
   },
   ignorePatterns: [
@@ -22,12 +27,57 @@ module.exports = {
     "/generated/**/*", // Ignore generated files.
   ],
   plugins: [
+    "react",
     "@typescript-eslint",
     "import",
+    "tailwindcss",
   ],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+    "@typescript-eslint/no-empty-interface": "warn",
+    "@typescript-eslint/no-empty-function": "warn",
+    "@typescript-eslint/ban-ts-comment": "warn",
+    "react-hooks/exhaustive-deps": "warn",
+    "no-var": "error",
+    "prefer-const": "warn",
+    "import/order": [
+      "warn",
+      {
+        "groups": [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling"],
+          "index",
+          "object",
+          "type"
+        ],
+        "alphabetize": {
+          "order": "asc",
+          "caseInsensitive": true
+        }
+      }
+    ],
+    "tailwindcss/no-custom-classname": "warn",
+    "tailwindcss/enforces-negative-arbitrary-values": "warn",
+    "tailwindcss/enforces-shorthand": "warn",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "react/jsx-uses-react": "off",
+    "react/jsx-uses-vars": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      typescript: {
+        directory: "./src",
+      },
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
   },
 };
