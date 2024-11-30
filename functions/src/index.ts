5bf8c15d5f8c2098fc3d14dff724bf4102ff3c4e
BumgeunSong <isp1195@gmail.com>
2024-11-30 23:09:03 +0900

    추가한 함수가 deploy되도록 index.ts에 추가한다

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,9 @@
-import { onCommentCreated } from './notifications/commentOnPost';
-
 import * as v2 from "firebase-functions/v2";
+import { onCommentCreated } from './notifications/commentOnPost';
 
 export const helloWorld = v2.https.onRequest((request, response) => {
   const name = request.query.name;
   response.send(`<h1>Hello ${name}!</h1>`);
 });
+
+export { onCommentCreated };
