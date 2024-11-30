f38fdce824e26b9ecb6e8ce8f41813c7465dd4f3
BumgeunSong <isp1195@gmail.com>
2024-12-01 00:32:49 +0900

    index.ts에 추가하기

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,10 @@
 import * as v2 from "firebase-functions/v2";
 import { onCommentCreated } from './notifications/commentOnPost';
 import { onReplyCreatedOnComment } from './notifications/replyOnComment';
+import { onReplyCreatedOnPost } from './notifications/replyOnPost';
 export const helloWorld = v2.https.onRequest((request, response) => {
   const name = request.query.name;
   response.send(`<h1>Hello ${name}!</h1>`);
 });
 
-export { onCommentCreated, onReplyCreatedOnComment };
+export { onCommentCreated, onReplyCreatedOnComment, onReplyCreatedOnPost };
