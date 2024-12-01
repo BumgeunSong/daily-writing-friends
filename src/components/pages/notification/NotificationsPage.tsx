c9ba1988a1b7555fde009cb07f39c46429d8646f
BumgeunSong <isp1195@gmail.com>
2024-12-01 20:58:59 +0900

    실제 데이터를 쿼리해서 쓰도록 변경

--- src/components/pages/notification/NotificationsPage.tsx
+++ src/components/pages/notification/NotificationsPage.tsx
@@ -5,14 +5,32 @@ import NotificationsHeader from './NotificationsHeader';
 import NotificationsList from './NotificationsList';
 import { Notification, NotificationType } from '@/types/Notification';
 import { Timestamp } from 'firebase/firestore';
+import { getNotifications } from '@/utils/notificationUtils';
+import { useQuery } from '@tanstack/react-query';
+import { useAuth } from '@/contexts/AuthContext';
+import StatusMessage from '@/components/common/StatusMessage';
+
 
 const NotificationsPage: React.FC = () => {
+  const { currentUser } = useAuth();
+
+  const { data: notifications, isLoading, isError } = useQuery(
+    ['notifications', currentUser?.uid],
+    () => getNotifications(currentUser?.uid),
+    {
+      enabled: !!currentUser?.uid, // currentUser가 있을 때만 쿼리 실행
+    }
+  );
+
+  if (isLoading) return <StatusMessage isLoading={isLoading} />;
+  if (isError) return <StatusMessage error={isError} />;
+
   return (
     <div className="flex flex-col h-[calc(100vh-4rem)]">
       <NotificationsHeader />
       <Card className="flex-1 overflow-hidden">
         <ScrollArea className="h-full">
-          <NotificationsList notifications={MOCK_NOTIFICATIONS} />
+          <NotificationsList notifications={notifications} />
         </ScrollArea>
       </Card>
     </div>
