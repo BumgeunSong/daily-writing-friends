a4516305233cfd04a87037ee5b0bb13b1dc491e6
BumgeunSong <isp1195@gmail.com>
2024-12-01 17:28:01 +0900

    문서 생성 트리거의 path를 올바르게 수정

--- functions/src/notifications/replyOnPost.ts
+++ functions/src/notifications/replyOnPost.ts
@@ -6,46 +6,46 @@ import { Post } from "../types/Post";
 import admin from "../admin";
 
 export const onReplyCreatedOnPost = onDocumentCreated(
-  "boards/{boardId}/posts/{postId}/replies/{replyId}",
-  async (event) => {
-    const reply = event.data?.data() as Reply;
+    "boards/{boardId}/posts/{postId}/comments/{commentId}/replies/{replyId}",
+    async (event) => {
+        const reply = event.data?.data() as Reply;
 
-    const postId = event.params.postId;
-    const boardId = event.params.boardId;
-    const replyAuthorId = reply.userId;
+        const postId = event.params.postId;
+        const boardId = event.params.boardId;
+        const replyAuthorId = reply.userId;
 
-    // 게시물 소유자 ID 가져오기
-    const postSnapshot = await admin
-        .firestore()
-        .doc(`boards/${boardId}/posts/${postId}`)
-        .get();
-    const postData = postSnapshot.data() as Post;
-    const postAuthorId = postData.authorId;
+        // 게시물 소유자 ID 가져오기
+        const postSnapshot = await admin
+            .firestore()
+            .doc(`boards/${boardId}/posts/${postId}`)
+            .get();
+        const postData = postSnapshot.data() as Post;
+        const postAuthorId = postData.authorId;
 
-    const message = `${reply.userName}님이 '${postData.title.slice(0, 10)}...' 글에 답글을 달았어요.`;
+        const message = `${reply.userName}님이 '${postData.title.slice(0, 10)}...' 글에 답글을 달았어요.`;
 
-    console.log("postAuthorId", postAuthorId);
-    console.log("replyAuthorId", replyAuthorId);
+        console.log("postAuthorId", postAuthorId);
+        console.log("replyAuthorId", replyAuthorId);
 
-    // 게시물 소유자에게 알림 생성
-    if (postAuthorId && postAuthorId !== replyAuthorId) {
-      const notification: Notification = {
-        type: NotificationType.REPLY_ON_POST,
-        fromUserId: replyAuthorId,
-        boardId: boardId,
-        postId: postId,
-        replyId: reply.id,
-        message: message,
-        timestamp: Timestamp.now(),
-        read: false,
-    };      
+        // 게시물 소유자에게 알림 생성
+        if (postAuthorId && postAuthorId !== replyAuthorId) {
+            const notification: Notification = {
+                type: NotificationType.REPLY_ON_POST,
+                fromUserId: replyAuthorId,
+                boardId: boardId,
+                postId: postId,
+                replyId: reply.id,
+                message: message,
+                timestamp: Timestamp.now(),
+                read: false,
+            };
 
-    // 글 작성자에게 알림 생성
-    console.log("Notification created", notification);
-    await admin
-        .firestore()
-        .collection(`users/${postAuthorId}/notifications`)
-        .add(notification);
+            // 글 작성자에게 알림 생성
+            console.log("Notification created", notification);
+            await admin
+                .firestore()
+                .collection(`users/${postAuthorId}/notifications`)
+                .add(notification);
+        }
     }
-  }
 );
