f2c0e73d98da30336c6247af112c909e016c374f
BumgeunSong <isp1195@gmail.com>
2024-12-03 00:00:57 +0900

    메시지에 들어가는 post 제목과 userNickName을 유저 쪽에서 fetch하도록 변경

--- src/components/pages/notification/NotificationItem.tsx
+++ src/components/pages/notification/NotificationItem.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Notification, NotificationType } from '@/types/Notification';
-import { MessageSquare } from 'lucide-react';
-import { Timestamp } from 'firebase/firestore';
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
-
+import { fetchPostTitle } from '@/utils/postUtils';
+import { fetchUserNickname } from '@/utils/userUtils';
 interface NotificationItemProps {
   notification: Notification;
 }
@@ -14,29 +13,57 @@ function getNotificationLink(notification: Notification): string {
 }
 
 export const NotificationItem = ({ notification }: NotificationItemProps) => {
-    return (
-      <Link to={getNotificationLink(notification)}>
-        <div
-          className={`flex cursor-pointer items-start gap-3 border-b px-4 py-3 transition-all hover:bg-accent/50 ${
-            !notification.read ? 'bg-accent/30' : ''
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    generateMessage(notification).then(setMessage);
+  }, [notification]);
+
+  return (
+    <Link to={getNotificationLink(notification)}>
+      <div
+        className={`flex cursor-pointer items-start gap-3 border-b px-4 py-3 transition-all hover:bg-accent/50 ${!notification.read ? 'bg-accent/30' : ''
           }`}
-        >
-          <Avatar>
-            <AvatarImage src={notification.fromUserProfileImage} alt="User Avatar" />
-            <AvatarFallback>
-              {notification.fromUserId.slice(0, 2).toUpperCase()}
-            </AvatarFallback>
-          </Avatar>
-          <div className='flex-1 space-y-1'>
-            <p className='text-sm font-medium leading-tight text-foreground'>
-              {notification.message}
-            </p>
-            <span className='text-[11px] text-muted-foreground/80'>
-              {notification.timestamp.toDate().toLocaleString()}
-            </span>
-          </div>
+      >
+        <Avatar>
+          <AvatarImage src={notification.fromUserProfileImage} alt="User Avatar" />
+          <AvatarFallback>
+            {notification.fromUserId.slice(0, 2).toUpperCase()}
+          </AvatarFallback>
+        </Avatar>
+        <div className='flex-1 space-y-1'>
+          <p className='text-sm font-medium leading-tight text-foreground'>
+            {message}
+          </p>
+          <span className='text-[11px] text-muted-foreground/80'>
+            {notification.timestamp.toDate().toLocaleString()}
+          </span>
         </div>
-      </Link>
-    );
-  };
-  
+      </div>
+    </Link>
+  );
+};
+
+
+const generateMessage = async (notification: Notification) => {
+  const postTitle = await fetchPostTitle(notification.boardId, notification.postId);
+  const postTitleSnippet = generateTitleSnippet(postTitle || '');
+
+  const userNickName = await fetchUserNickname(notification.fromUserId);
+
+  switch (notification.type) {
+    case NotificationType.COMMENT_ON_POST:
+      return `${userNickName}님이 ${postTitleSnippet} 글에 댓글을 달았어요.`;
+    case NotificationType.REPLY_ON_COMMENT:
+      return `${userNickName}님이 ${postTitleSnippet} 댓글에 답글을 달았어요.`;
+    case NotificationType.REPLY_ON_POST:
+      return `${userNickName}님이 ${postTitleSnippet} 글에 답글을 달았어요.`;
+  }
+}
+
+const generateTitleSnippet = (contentTitle: string) => {
+  if (contentTitle.length > 12) {
+    return contentTitle.slice(0, 12) + "...";
+  }
+  return contentTitle;
+}
