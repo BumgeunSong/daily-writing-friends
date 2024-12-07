d5ca6e3b719b0580362f89414b4ff4cd58bc1888
BumgeunSong <isp1195@gmail.com>
2024-12-07 23:29:15 +0900

    Header 타이틀과 설정 버튼의 세로 정렬

--- src/components/pages/notification/NotificationsHeader.tsx
+++ src/components/pages/notification/NotificationsHeader.tsx
@@ -6,11 +6,11 @@ export const NotificationsHeader: React.FC = () => {
     return (
         <div className='h-16 mb-4 flex items-center justify-between px-4'>
             <div className='flex items-center gap-2'>
-                <Bell className='size-5' />
+                <Bell className='w-6 h-6' />
                 <h1 className='text-2xl font-bold'>알림</h1>
             </div>
-            <Link to="/notifications/settings">
-                <Settings className='size-5 text-gray-600 hover:text-gray-900 transition-colors' />
+            <Link to="/notifications/settings" className='flex items-center'>
+                <Settings className='w-6 h-6 text-gray-600 hover:text-gray-900 transition-colors' />
             </Link>
         </div>
     );
