c83ca9f4c748c8755215cf4b3d83e0f2a3795542
BumgeunSong <isp1195@gmail.com>
2024-12-10 23:51:59 +0900

    유저 동의를 얻고 토큰을 서버에 업데이트하는 로직 추가

--- src/utils/tokenUntils.ts
+++ src/utils/tokenUntils.ts
@@ -3,14 +3,8 @@ import { firestore } from "@/firebase";
 import { DeviceToken } from "@/messaging/DeviceToken";
 import { addDoc, collection, Timestamp } from "firebase/firestore";
 
-export const sendTokenToServer = async (token: string) => {
-    // create 'deviceToken' collection as sub-collection of 'users' collection
-    // get user id from useAuth
-    const { currentUser } = useAuth();
-    if (!currentUser) {
-        throw new Error("User not found");
-    }
-    const deviceTokenCollection = collection(firestore, `users/${currentUser.uid}/deviceTokens`);
+export const sendTokenToServer = async (userId: string, token: string) => {
+    const deviceTokenCollection = collection(firestore, `users/${userId}/deviceTokens`);
     const deviceToken: DeviceToken = {
         token,
         timestamp: Timestamp.now(),
