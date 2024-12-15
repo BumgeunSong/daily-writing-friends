fe590aed316119383f8d8372789e43bfb05afb50
BumgeunSong <isp1195@gmail.com>
2024-12-15 21:47:50 +0900

    새로 만든 함수 export

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import { onCommentCreated } from './notifications/commentOnPost';
 import { onReplyCreatedOnComment } from './notifications/replyOnComment';
 import { onReplyCreatedOnPost } from './notifications/replyOnPost';
+import { onNotificationCreated } from './notifications/sendMessageOnNotification';
 
-export { onCommentCreated, onReplyCreatedOnComment, onReplyCreatedOnPost };
+export { onCommentCreated, onReplyCreatedOnComment, onReplyCreatedOnPost, onNotificationCreated };
