980e1846c4454f99ae8b32a00f7920f418da08d4
BumgeunSong <isp1195@gmail.com>
2024-12-21 20:10:18 +0900

    데이터가 업로드 되었다면 새로 고침을 하도록 안내

--- src/hooks/useUpdateUserData.tsx
+++ src/hooks/useUpdateUserData.tsx
@@ -3,7 +3,8 @@ import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { updateUserData } from '../utils/userUtils';
 import { storage } from '../firebase';
 import { useState } from 'react';
-import { useToast } from '../hooks/use-toast';
+import { useToast } from './use-toast';
+import { ToastAction } from '@/components/ui/toast';
 
 export function useUpdateUserData(
   userId: string,
@@ -12,7 +13,7 @@ export function useUpdateUserData(
 ) {
   const navigate = useNavigate();
   const [isLoading, setLoading] = useState<boolean>(false);
-  const toast = useToast();
+  const { toast } = useToast();
 
   const uploadProfilePhoto = async (file: File, userId: string) => {
     const storageRef = ref(storage, `profilePhotos/${userId}`);
@@ -32,13 +33,20 @@ export function useUpdateUserData(
         await updateUserData(userId, { profilePhotoURL: downloadURL });
       }
 
-      toast.toast({
-        description: '정보가 성공적으로 업데이트되었습니다.'
+      toast({
+        title: '정보가 성공적으로 업데이트되었습니다.',
+        description: '정보가 바뀌지 않으면 새로고침을 해주세요.',
+        action: (
+          <ToastAction altText='새로고침' onClick={() => window.location.reload()}>
+            새로고침
+          </ToastAction>
+        ),
       });
+
       navigate('/account');
     } catch (error) {
       console.error('Error updating account information:', error);
-      toast.toast({
+      toast({
         description: '정보를 업데이트하는 중 오류가 발생했습니다. 다시 시도해주세요.'
       });
     } finally {
