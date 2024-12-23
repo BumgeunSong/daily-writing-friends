455db707cd082d9d05068e0d7e42c323808d2caf
BumgeunSong <isp1195@gmail.com>
2024-12-23 18:17:06 +0900

    firstDay 필드를 추가

--- src/types/Board.ts
+++ src/types/Board.ts
@@ -1,7 +1,10 @@
+import { Timestamp } from "firebase/firestore";
+
 // src/types/Board.ts
 export interface Board {
   id: string;
   title: string;
   description: string;
   createdAt: Date;
+  firstDay?: Timestamp;
 }
