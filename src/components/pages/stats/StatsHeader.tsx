27887e271e0bab54202b918e5d87552d44435c7a
BumgeunSong <isp1195@gmail.com>
2024-12-31 10:05:04 +0900

    헤더를 기록으로 변경한다

--- src/components/pages/stats/StatsHeader.tsx
+++ src/components/pages/stats/StatsHeader.tsx
@@ -1,8 +1,8 @@
 const StatsHeader = () => {
     return (
         <header className='bg-primary py-4 text-primary-foreground'>
-            <div className='container mx-auto flex items-center px-6'>
-                <h1 className='text-2xl font-bold sm:text-3xl'>프렌즈</h1>
+            <div className='container px-4'>
+                <h1 className='text-2xl font-bold sm:text-3xl pl-2'>기록</h1>
             </div>
         </header>
     );
