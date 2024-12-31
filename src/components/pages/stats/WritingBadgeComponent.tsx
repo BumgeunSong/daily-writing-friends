8ff46d3e0e04855de67879820bd7ba7608b288ad
BumgeunSong <isp1195@gmail.com>
2024-12-31 13:03:38 +0900

    뱃지 디자인을 PostCard 디자인과 일관되게 수정

--- src/components/pages/stats/WritingBadgeComponent.tsx
+++ src/components/pages/stats/WritingBadgeComponent.tsx
@@ -4,11 +4,18 @@ import { Badge } from "@/components/ui/badge"
 export interface WritingBadgeComponentProps {
     badge: WritingBadge
 }
+
 export function WritingBadgeComponent({ badge }: WritingBadgeComponentProps) {
     return (
-        <Badge id={badge.name} className="bg-gray-200">
-            <span>{badge.emoji} </span>
-            <span>{badge.name}</span>
+        <Badge 
+            id={badge.name} 
+            variant="secondary"
+            className="flex items-center gap-1 px-2 py-1 rounded-full"
+        >
+            <span className="text-xs leading-none">{badge.emoji}</span>
+            <span className="text-xs font-normal leading-none">
+                {badge.name}
+            </span>
         </Badge>
     )
 }
