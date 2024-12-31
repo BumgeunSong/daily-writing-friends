f80a38aa4a08f085fa96f0b4398d304a8a994076
BumgeunSong <isp1195@gmail.com>
2024-12-31 12:27:34 +0900

    뱃지들이 닉네임 아래에 위치하고, 공간이 부족할 경우 자연스럽게 줄바꿈

--- src/components/pages/stats/UserStatsCard.tsx
+++ src/components/pages/stats/UserStatsCard.tsx
@@ -15,20 +15,22 @@ export function UserStatsCard({ stats }: UserStatsCardProps) {
       <Card className="w-full">
         <CardContent className="flex items-start gap-4 p-4">
           <div className="flex flex-1 items-start gap-4">
-            <Avatar className="h-12 w-12">
+            <Avatar className="h-12 w-12 shrink-0">
               <AvatarImage src={user.profilePhotoURL || undefined} alt={user.nickname || "User"} />
               <AvatarFallback>{user.nickname?.[0] || user.realname?.[0] || "U"}</AvatarFallback>
             </Avatar>
-            <div className="flex flex-col gap-2">
-              <div>
-                <h3 className="font-semibold">{user.nickname || user.realname || "Anonymous"}</h3>
+            <div className="flex flex-col gap-1.5 min-w-0">
+              <h3 className="font-semibold truncate">
+                {user.nickname || user.realname || "Anonymous"}
+              </h3>
+              <div className="flex flex-wrap gap-1">
                 {stats.badges.map((badge) => (
                   <WritingBadgeComponent key={badge.name} badge={badge} />
                 ))}
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-end gap-2">
+          <div className="flex flex-col items-end gap-2 shrink-0">
             <ContributionGraph 
               contributions={contributions} 
               className="w-24"
