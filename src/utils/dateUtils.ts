60c0722881287874d16edec3a7395774dccd15fa
BumgeunSong <isp1195@gmail.com>
2025-01-05 22:03:36 +0900

    timezone을 파라미터로 넘길 수 있도록 수정

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,5 +1,10 @@
 import { WritingHistory } from "@/types/\bWritingHistory";
 
+enum TimeZone {
+    KST = 'Asia/Seoul',
+    UTC = 'UTC'
+}
+
 /**
  * KST를 기준으로 주어진 날짜가 영업일인지 확인합니다 (주말과 공휴일 제외)
  * @param date - Date 객체
@@ -7,77 +12,75 @@ import { WritingHistory } from "@/types/\bWritingHistory";
  * @throws Error - 유효하지 않은 Date 객체인 경우
  */
 export function isWorkingDay(date: Date): boolean {
-  if (!(date instanceof Date) || isNaN(date.getTime())) {
-    throw new Error('Invalid Date object');
-  }
+    if (!(date instanceof Date) || isNaN(date.getTime())) {
+        throw new Error('Invalid Date object');
+    }
 
-  if (isWeekendByKST(date)) {
-    return false;
-  }
+    if (isWeekendByKST(date, TimeZone.KST)) {
+        return false;
+    }
 
-  if (isHolidayByKST(date)) {
-    return false;
-  }
+    if (isHolidayByKST(date, TimeZone.KST)) {
+        return false;
+    }
 
-  return true;
-} 
+    return true;
+}
 
 interface Holiday {
     date: Date;
-  }
-  
-  const HOLIDAYS: Holiday[] = [
+}
+
+const HOLIDAYS: Holiday[] = [
     { date: new Date('2024-12-31T00:00:00Z') },
     { date: new Date('2025-01-01T00:00:00Z') },
     { date: new Date('2025-01-28T00:00:00Z') },
     { date: new Date('2025-01-29T00:00:00Z') },
     { date: new Date('2025-01-30T00:00:00Z') }
-  ];
-  
-  function isHolidayByKST(date: Date): boolean {
-    return HOLIDAYS.some(holiday => isSameDayByKST(holiday.date, date));
-  }
-  
-  function isWeekendByKST(date: Date): boolean {
-      const kstDate = new Date(date.toLocaleString('en-US', {
-          timeZone: 'Asia/Seoul'
-      }));
-      const dayOfWeek = kstDate.getDay();
-      return dayOfWeek === 0 || dayOfWeek === 6;
+];
+
+function isHolidayByKST(date: Date, timeZone: TimeZone): boolean {
+    return HOLIDAYS.some(holiday => isSameDay(holiday.date, date, timeZone));
+}
+
+function isWeekendByKST(date: Date, timeZone: TimeZone): boolean {
+    const dateInTimeZone = new Date(date.toLocaleString('en-US', { timeZone }));
+    const dayOfWeek = dateInTimeZone.getDay();
+    return dayOfWeek === 0 || dayOfWeek === 6;
 }
 
 export function getContentLengthOfWorkingDays(writingHistory: WritingHistory[], workingDays: Date[]): number[] {
     return workingDays
-    .map(workingDay => {
-      const history = writingHistory.find(history => {
-        return isSameDayByKST(history.createdAt.toDate(), workingDay);
-      });
-      return history?.post?.contentLength ?? 0;
-    });
-  }
+        .map(workingDay => {
+            const history = writingHistory.find(history => {
+                return isSameDay(history.createdAt.toDate(), workingDay, TimeZone.KST);
+            });
+            return history?.post?.contentLength ?? 0;
+        });
+}
 
 export function getRecentWorkingDays(count: number): Date[] {
-  const workingDays: Date[] = [];
-  const currentDate = new Date();
+    const workingDays: Date[] = [];
+    const currentDate = new Date();
 
-  while (workingDays.length < count) {
-    if (isWorkingDay(currentDate)) {
-      workingDays.push(currentDate);
+    while (workingDays.length < count) {
+        if (isWorkingDay(currentDate)) {
+            workingDays.push(currentDate);
+        }
+        currentDate.setDate(currentDate.getDate() - 1);
     }
-    currentDate.setDate(currentDate.getDate() - 1);
-  }
 
-  return workingDays.reverse();
+    return workingDays.reverse();
 }
 
 // 두 날짜가 같은 연도, 같은 달, 같은 날인지 확인합니다 (KST 기준)
-export function isSameDayByKST(date1: Date, date2: Date): boolean {
-    const kstDate1 = new Date(date1.toLocaleString('en-US', { timeZone: 'Asia/Seoul' }));
-    const kstDate2 = new Date(date2.toLocaleString('en-US', { timeZone: 'Asia/Seoul' }));
+export function isSameDay(date1: Date, date2: Date, timeZone: TimeZone): boolean {
+    const date1InTimeZone = new Date(date1.toLocaleString('en-US', { timeZone }));
+    const date2InTimeZone = new Date(date2.toLocaleString('en-US', { timeZone }));
 
     return (
-        kstDate1.getFullYear() === kstDate2.getFullYear() &&
-        kstDate1.getMonth() === kstDate2.getMonth() &&
-        kstDate1.getDate() === kstDate2.getDate()
+        date1InTimeZone.getFullYear() === date2InTimeZone.getFullYear() &&
+        date1InTimeZone.getMonth() === date2InTimeZone.getMonth() &&
+        date1InTimeZone.getDate() === date2InTimeZone.getDate()
     );
 }
