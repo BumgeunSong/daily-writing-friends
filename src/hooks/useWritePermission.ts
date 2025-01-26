4edd67bbb3a7d74c59d4c779c59833ccc16cfe8e
BumgeunSong <isp1195@gmail.com>
2025-01-26 22:35:54 +0900

    import 문 수정

--- src/hooks/useWritePermission.ts
+++ src/hooks/useWritePermission.ts
@@ -2,13 +2,13 @@ import { firestore } from "@/firebase";
 import { User } from "@/types/User";
 import { doc, getDoc } from "firebase/firestore";
 import { useQuery } from '@tanstack/react-query';
-import Sentry from '@sentry/react';
+import { captureException } from '@sentry/react';
 
-const useWritePermission = (userId: string | null, boardId: string) => {
+export default function useWritePermission(userId: string | null, boardId: string) {
     if (userId === null) {
         const noUserIdError = new Error('유저 ID가 존재하지 않아 유저 데이터를 불러올 수 없습니다.');
         console.error(noUserIdError);
-        Sentry.captureException(noUserIdError);
+        captureException(noUserIdError);
         return { writePermission: false, isLoading: false, error: noUserIdError };
     }
 
@@ -25,5 +25,3 @@ const useWritePermission = (userId: string | null, boardId: string) => {
 
     return { writePermission, isLoading, error };
 }
-
-export default useWritePermission;
