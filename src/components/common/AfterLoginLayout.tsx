1b4ab19202f57512c1d2eda1d7cdb4f9dc983244
BumgeunSong <isp1195@gmail.com>
2025-03-31 12:56:26 +0900

    Revert "Merge pull request #178 from BumgeunSong/feat/offline"
    This reverts commit 47b32bad2ddb825c89a6a0ed6a77d99549765bb9, reversing
    changes made to 4076fdcc3695a3205c83d09000ccf28153157e8e.

--- src/components/common/AfterLoginLayout.tsx
+++ src/components/common/AfterLoginLayout.tsx
@@ -1,18 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { Toaster } from '../ui/toaster';
 import BottomTabsNavigator from '../pages/BottomTabsNavigator';
-import OfflineBanner from './OfflineBanner';
 
-export const AfterLoginLayout = () => {
-  
-  return (
+export const AfterLoginLayout = () => (
     <div className='flex min-h-screen flex-col pb-16 safe-top safe-right safe-bottom safe-left'>
-      <OfflineBanner />
-      <div className='grow'>
-        <Outlet />
-      </div>
-      <Toaster />
-      <BottomTabsNavigator />
+        <div className='grow'>
+            <Outlet />
+        </div>
+        <Toaster />
+        <BottomTabsNavigator />
     </div>
-  );
-};
+);
--- src/components/pages/board/BoardHeader.tsx
+++ src/components/pages/board/BoardHeader.tsx
@@ -1,25 +1,20 @@
-import { ChevronDown, WifiOff } from 'lucide-react';
+import { ChevronDown } from 'lucide-react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { fetchBoardTitle } from '../../../utils/boardUtils';
 import StatusMessage from '../../common/StatusMessage';
-import { useOnlineStatus } from '@/hooks/useOnlineStatus';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 
 interface BoardHeaderProps {
   boardId?: string;
 }
 
 const BoardHeader: React.FC<BoardHeaderProps> = React.memo(({ boardId }) => {
-  const isOnline = useOnlineStatus();
   const { data: title, isLoading, error } = useQuery(
     ['boardTitle', boardId],
     () => fetchBoardTitle(boardId || ''),
     {
       enabled: !!boardId, // boardId가 있을 때만 쿼리 실행
-      staleTime: 5 * 60 * 1000, // 5분
-      cacheTime: 24 * 60 * 60 * 1000, // 24시간
     }
   );
 
@@ -41,22 +36,6 @@ const BoardHeader: React.FC<BoardHeaderProps> = React.memo(({ boardId }) => {
           <span className='text-2xl font-bold sm:text-3xl'>{title || '타이틀 없음'}</span>
           <ChevronDown className='size-5' />
         </Link>
-        
-        {/* 오프라인 상태 인디케이터 */}
-        {!isOnline && (
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <div className="flex items-center bg-primary-foreground/10 px-3 py-1.5 rounded-full">
-                  <WifiOff className="size-4 mr-2" />
-                </div>
-              </TooltipTrigger>
-              <TooltipContent side="bottom">
-                <p>오프라인 모드: 캐시된 데이터만 표시됩니다</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        )}
       </div>
     </header>
   );
