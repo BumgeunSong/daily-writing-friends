073268ea80cada996cbf7e19e36d6fb55f96566b
BumgeunSong <isp1195@gmail.com>
2025-04-10 21:39:33 +0900

    지금 함께하는 명수 추가

--- src/components/pages/join/CountdownSection.tsx
+++ src/components/pages/join/CountdownSection.tsx
@@ -5,6 +5,7 @@ interface CountdownSectionProps {
 }
 
 const CountdownSection: React.FC<CountdownSectionProps> = ({ daysRemaining }) => (
+  <>
   <section className="py-4">
     <p className="text-lg font-medium">다음 기수 시작까지</p>
     <div className="flex items-center mt-2">
@@ -12,6 +13,15 @@ const CountdownSection: React.FC<CountdownSectionProps> = ({ daysRemaining }) =>
       <span className="text-2xl ml-2">일</span>
     </div>
   </section>
+
+  <section className="py-4">
+    <p className="text-lg font-medium">지금 매일 쓰고 있는 사람</p>
+    <div className="flex items-center mt-2">
+      <span className="text-4xl font-bold">19</span>
+        <span className="text-2xl ml-2">명</span>
+      </div>
+    </section>
+  </>
 );
 
 export default CountdownSection; 
