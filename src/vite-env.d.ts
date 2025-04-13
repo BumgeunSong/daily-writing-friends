9d7b52c489310fbac4fb0ce31c2f85fc94b3e0cc
BumgeunSong <isp1195@gmail.com>
2025-04-13 17:36:06 +0900

    구글 애널리틱스 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,6 +7,7 @@ interface ImportMetaEnv {
     readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
     readonly VITE_FIREBASE_APP_ID: string;
     readonly VITE_USE_FIREBASE_EMULATOR: string;
+    readonly VITE_FIREBASE_MEASUREMENT_ID: string;
     // 기타 환경 변수...
   }
 
