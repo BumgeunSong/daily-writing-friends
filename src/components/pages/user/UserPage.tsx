bc6115e2b37be289b1a4cc07f41e23f3690306c1
BumgeunSong <isp1195@gmail.com>
2025-04-29 09:54:19 +0900

    불필요한 current user 삭제

--- src/components/pages/user/UserPage.tsx
+++ src/components/pages/user/UserPage.tsx
@@ -1,11 +1,9 @@
 import { useParams } from 'react-router-dom';
 import UserProfile from './UserProfile';
 import UserActivityTab from './UserActivityTab';
-import { useAuth } from '@/contexts/AuthContext';
 
 export default function UserPage() {
   const { userId } = useParams<{ userId: string }>();
-  const { currentUser } = useAuth();
 
   if (!userId) {
     return (
