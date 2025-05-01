417ddeb960dd3253fcbae81659fc47dd52369171
BumgeunSong <isp1195@gmail.com>
2025-05-01 16:57:06 +0900

    메모이제이션 제거

--- src/components/pages/board/BoardHeader.tsx
+++ src/components/pages/board/BoardHeader.tsx
@@ -9,7 +9,7 @@ interface BoardHeaderProps {
   boardId?: string;
 }
 
-const BoardHeader: React.FC<BoardHeaderProps> = React.memo(({ boardId }) => {
+const BoardHeader: React.FC<BoardHeaderProps> = ({ boardId }) => {
   const { data: title, isLoading, error } = useQuery(
     ['boardTitle', boardId],
     () => fetchBoardTitle(boardId || ''),
@@ -39,6 +39,6 @@ const BoardHeader: React.FC<BoardHeaderProps> = React.memo(({ boardId }) => {
       </div>
     </header>
   );
-});
+};
 
 export default BoardHeader;
