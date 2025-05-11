1d15169c4d61328d860ac95ba136f0a61e83f51b
BumgeunSong <isp1195@gmail.com>
2025-05-11 17:11:19 +0900

    Provider 유틸 함수 추가

---
+++ src/test/utils/renderWithProviders.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MemoryRouter } from 'react-router-dom';
+import { AuthProvider } from '@/shared/hooks/useAuth';
+import { HelmetProvider } from 'react-helmet-async';
+import { NavigationProvider } from '@/shared/contexts/NavigationContext';
+import { BottomTabHandlerProvider } from '@/shared/contexts/BottomTabHandlerContext';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false, // 테스트에서 자동 재시도 방지
+    },
+  },
+});
+
+export function renderWithProviders(
+    ui: React.ReactElement,
+    { route = '/' }: { route?: string } = {}
+  ) {
+    window.history.pushState({}, 'Test page', route);
+    return render(
+      <HelmetProvider>
+        <MemoryRouter initialEntries={[route]}>
+          <QueryClientProvider client={queryClient}>
+            <AuthProvider>
+              <NavigationProvider 
+                debounceTime={500} 
+                topThreshold={30} 
+                ignoreSmallChanges={10}
+              >
+                <BottomTabHandlerProvider>
+                  {ui}
+                </BottomTabHandlerProvider>
+              </NavigationProvider>
+            </AuthProvider>
+          </QueryClientProvider>
+        </MemoryRouter>
+      </HelmetProvider>
+    );
+  }
