aa31071bbeff4e82ff85b79b3025b98cb8195a4c
BumgeunSong <isp1195@gmail.com>
2025-05-12 15:13:56 +0900

    json-summary reporter 포맷 생성

--- vite.config.ts
+++ vite.config.ts
@@ -86,7 +86,7 @@ export default defineConfig(({ mode }) => {
       include: ['src/**/*.{test,spec}.{js,jsx,ts,tsx}'],
       coverage: {
         provider: 'v8',
-        reporter: ['text', 'json', 'html'],
+        reporter: ['text', 'lcov', 'json-summary'],
         exclude: ['node_modules/', 'src/setupTest.ts']
       },
       deps: {
