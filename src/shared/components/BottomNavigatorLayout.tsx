f22678bc004125c93e7d59f98aa553fa413400b3
BumgeunSong <isp1195@gmail.com>
2025-05-15 11:46:20 +0900

    safe 에어리어 커스텀 클래스 제거

--- src/shared/components/BottomNavigatorLayout.tsx
+++ src/shared/components/BottomNavigatorLayout.tsx
@@ -3,7 +3,7 @@ import { Toaster } from '@shared/ui/toaster';
 import BottomTabsNavigator from './BottomTabsNavigator';
 
 export const BottomNavigatorLayout = () => (
-    <div className='safe-top safe-right safe-bottom safe-left flex min-h-screen flex-col pb-16'>
+    <div className='flex min-h-screen flex-col pb-16'>
         <div className='grow'>
             <Outlet />
         </div>
