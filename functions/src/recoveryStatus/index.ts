689db0a1720a40dd8c59bc33378bf389eded34b9
BumgeunSong <isp1195@gmail.com>
2025-10-29 18:00:32 +0800

    Delete Phase 1 recoveryStatus module

--- functions/src/recoveryStatus/index.ts
+++
@@ -1,9 +0,0 @@
-// Recovery Status-related Cloud Functions
-
-// Handlers
-export { updateRecoveryStatusOnMidnightV2 } from './updateRecoveryStatusOnMidnightV2';
-export * from './stateTransitions';
-export * from './streakUtils';
-
-// Types
-export * from './StreakInfo';
